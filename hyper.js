a38ea7a6f83bc4c058f70cf6d8d297167ef51f95
Raghav Subramaniam <raghavs511@gmail.com>
2017-02-07 20:25:52 -0800

    Add hyper config

---
+++ hyper.js
@@ -0,0 +1,12 @@
+module.exports = {
+    config: {
+        fontSize: 14,
+        fontFamily: '"Fira Code", monospace',
+        cursorShape: 'BLOCK',
+        bell: false,
+    },
+    plugins: [
+        'hyperminimal',
+        'hyper-zenburn'
+    ]
+};
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+# variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf"    # list of files/folders to symlink in homedir
+dir=~/dotfiles # dotfiles directory
+files="hyper.js tmux.conf vimrc zshrc" # list of files/folders to symlink in homedir
 
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
