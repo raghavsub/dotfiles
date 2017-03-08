ae71e9a0a5a7a9433793d1af055d5859dce9e31a
Raghav Subramaniam <raghavs511@gmail.com>
2017-03-07 18:08:16 -0800

    Add symlinking script

---
+++ makesymlinks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dir=~/dotfiles
+
+files="bashrc
+    config/dunst/dunstrc
+    config/i3/config
+    config/i3/polybar
+    config/mopidy/mopidy.conf
+    config/streamlink/config
+    config/zathura/zathurarc
+    vimrc
+    Xresources"
+
+for file in $files; do
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
